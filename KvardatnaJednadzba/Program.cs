b408eb42e57763ed47dbda2ac8a1ae9e868991f4
Ivan Sokolović <isokolovic1@gmail.com>
2021-11-04 18:18:15 +0100

    Add project files.

---
+++ KvardatnaJednadzba/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvardatnaJednadzba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation qe = new QuadraticEquation(1, 2, -3);
+            Console.WriteLine(qe.A);
+
+            //qe.A = 10;
+
+            //Console.WriteLine(qe.A);
+
+            var roots = qe.Roots;
+
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+            qe = new QuadraticEquation(-1, -2, -3);
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ KvardatnaJednadzba/QuadraticEquation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvardatnaJednadzba
+{
+    class QuadraticEquation
+    {
+        public QuadraticEquation() //objekt će imati defaultne vrijednosti, za double = 0
+        {
+
+        }
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a; //potrebno inicijalizirati
+            this.b = b;
+            this.c = c;
+        }
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; } //value = po konvenciji
+        }
+
+        public double Y(double x) //Kvadratna jednadžba
+        {
+            return a * x * x + b * x + c;
+        }
+
+        public double Discriminant
+        {
+            get { return b * b - 4 * a * c; }
+        }
+
+        public double[] Roots
+        {
+            get
+            {
+                if(a == 0)
+                {
+                    return new double[] { -c / b, -c / b };
+                }
+
+                double sqrtDisc = Math.Sqrt(Discriminant);
+                return new double[] { (-b - sqrtDisc) / 2 / a , 
+                    (-b + sqrtDisc) / 2 / a };
+            }
+        }
+
+        private double a;
+        private double b;
+        private double c;
+
+    }
+}
